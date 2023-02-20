032c5ad5ef415519a9d9a1a1d6eca8e0a280bd9d
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-20 23:37:02 +0800

    Finish censorer microservice

---
+++ microservices/censorer/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Filter = require("bad-words");
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const filter = new Filter();
+
+app.post("/", (req, res) => {
+  const canonicalMsg = req.body.message;
+  if (canonicalMsg === undefined) {
+    res.status(400);
+    res.send({ message: "message is invalid" });
+    return;
+  }
+  let sanitised;
+  if (filter.isProfane(canonicalMsg)) {
+    sanitised = filter.clean(canonicalMsg);
+  } else {
+    sanitised = canonicalMsg;
+  }
+  console.log(`${canonicalMsg} -> ${sanitised}`);
+  res.send({ sanitised });
+});
+
+app.listen(port, () => {
+  console.log(`Censorer listening on port ${port}`);
+});
