45ac75cb3f7eb8ed9425be7ba57216bc2c2be712
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-21 18:45:44 +0800

    Update index.js

--- microservices/censorer/index.js
+++ microservices/censorer/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Filter = require("bad-words");
 
+const PORT = 3000;
+
 const app = express();
-const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -26,6 +27,6 @@ app.post("/", (req, res) => {
   res.send({ sanitised });
 });
 
-app.listen(port, () => {
-  console.log(`Censorer listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Censorer listening on port ${PORT}`);
 });
