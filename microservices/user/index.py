be013628564d06dd96f4615b57f1ac52db068e0f
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-26 23:37:45 +0800

    Update user microservice to use graphql

--- microservices/user/index.py
+++ microservices/user/index.py
@@ -29,6 +29,90 @@ class User(db.Model):
 
 db.create_all()
 
+
+def getUser_resolver(obj, info, name):
+    try:
+        user = User.query.get(name)
+        payload = {
+            "success": True,
+            "post": user.json()
+        }
+    except AttributeError:
+        payload = {
+            "success": False,
+            "errors": ["User item matching {name} not found"]
+        }
+    return payload
+
+
+class ContactNotValidError(Exception):
+    pass
+
+
+def create_user_resolver(obj, info, name, contact_no, email):
+    try:
+        normalised_email = validate_email(email)
+        if not re.match(r'\b[689]\d{7}\b', contact_no):
+            raise ContactNotValidError
+        user = User(name, contact_no, email=normalised_email)
+        db.session.add(user)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "user": user.json()
+        }
+    except EmailNotValidError:
+        payload = {
+            "success": False,
+            "errors": ["email is invalid"]
+        }
+    except ContactNotValidError:
+        payload = {
+            "success": False,
+            "errors": ["contact_no is invalid"]
+        }
+    return payload
+
+
+def update_user_resolver(obj, info, name, contact_no, email):
+    try:
+        user = User.query.get(name)
+        if user:
+            normalised_email = validate_email(email)
+            if not re.match(r'\b[689]\d{7}\b', contact_no):
+                raise ContactNotValidError
+            user.contact_no = contact_no
+            user.email = normalised_email
+        db.session.add(user)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "user": user.json()
+        }
+    except EmailNotValidError:
+        payload = {
+            "success": False,
+            "errors": ["email is invalid"]
+        }
+    except ContactNotValidError:
+        payload = {
+            "success": False,
+            "errors": ["contact_no is invalid"]
+        }
+    except AttributeError:
+        payload = {
+            "success": False,
+            "errors": ["User item matching {name} not found"]
+        }
+    return payload
+
+
+query = ObjectType("Query")
+mutation = ObjectType("Mutation")
+query.set_field("getUser", getUser_resolver)
+mutation.set_field("createUser", create_user_resolver)
+mutation.set_field("updateUser", update_user_resolver)
+
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(type_defs, snake_case_fallback_resolvers)
 
@@ -36,83 +120,10 @@ schema = make_executable_schema(type_defs, snake_case_fallback_resolvers)
 @app.post("/graphql")
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(schema)
-
-
-@app.post("/create")
-def create_new_user():
-    data = request.get_json()
-    user = User(**data)
-    try:
-        v = validate_email(user.email)
-        user.email = v["email"]
-    except EmailNotValidError:
-        return jsonify({
-            "message": "email is invalid"
-        }), 400
-    if not re.match(r'\b[689]\d{7}\b', user.contact_no):
-        return jsonify({
-            "message": "contact_no is invalid"
-        }), 400
-    if User.query.filter_by(name=user.name).first():
-        return jsonify({
-            "message": "name already exists"
-        }), 400
-    try:
-        db.session.add(user)
-        db.session.commit()
-    except:
-        return jsonify({
-            "message": "An internal server error occurred"
-        }), 500
-    return jsonify({
-        "data": user.json()
-    }), 201
-
-
-@app.get("/read/<string:name>")
-def get_user(name):
-    user = User.query.filter_by(name=name).first()
-    if user:
-        return jsonify({
-            "data": user.json()
-        })
-    return jsonify({
-        "message": "User not found"
-    }), 404
-
-
-@app.post("/update")
-def update_user_data():
-    try:
-        data = request.get_json()
-        user = User.query.filter_by(name=data["name"]).first()
-        if not user:
-            return jsonify({
-                "message": "User not found"
-            }), 404
-        try:
-            v = validate_email(data["email"])
-            user.email = v["email"]
-        except EmailNotValidError:
-            return jsonify({
-                "message": "email is invalid"
-            }), 400
-        if not re.match(r'\b[689]\d{7}\b', data["contact_no"]):
-            return jsonify({
-                "message": "contact_no is invalid"
-            }), 400
-        user.contact_no = data["contact_no"]
-        db.session.commit()
-        return jsonify(
-            {
-                "data": user.json()
-            }
-        )
-    except:
-        return jsonify({
-            "message": "An internal server error occurred"
-        }), 500
+    success, result = graphql_sync(
+        schema, data, context_value=request, debug=app.debug)
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 
 if __name__ == "__main__":
