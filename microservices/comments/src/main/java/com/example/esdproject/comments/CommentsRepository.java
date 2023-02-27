23117e370319d04b1293a5aaeb963b0c332a312c
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-27 10:47:24 +0800

    E

--- microservices/comments/src/main/java/com/example/esdproject/comments/CommentsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.esdproject.comments;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentsRepository extends CrudRepository<Comments, Integer> {
-}
