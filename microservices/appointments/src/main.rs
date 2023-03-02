3503ede82b7c5521d91cf8013b3b14e84a374272
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-03-02 18:34:47 +0800

    E

--- microservices/appointments/src/main.rs
+++ microservices/appointments/src/main.rs
@@ -6,13 +6,21 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use futures::TryStreamExt;
-use mongodb::{bson::doc, options::ClientOptions, Client, Collection};
+use futures::{stream, StreamExt, TryStreamExt};
+use mongodb::{
+    bson::{doc, DateTime},
+    options::ClientOptions,
+    Client, Collection,
+};
+use reqwest::Client as HttpClient;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::net::SocketAddr;
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    time::Duration,
+};
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
-use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,17 +33,25 @@ async fn main() -> Result<()> {
         .init();
     let client_options = ClientOptions::parse(&std::env::var("DB_URI")?).await?;
     let client = Client::with_options(client_options)?;
-    let database = client.database("appointments");
-    let collection = database.collection::<Appointment>("appointments");
+    let database =
+        client.database(&std::env::var("DATABASE").unwrap_or_else(|_| "esdproject".into()));
+    let collection = database.collection::<Appointment>(
+        &std::env::var("COLLECTION").unwrap_or_else(|_| "appointments".into()),
+    );
+    let shared_state = SharedState {
+        db: collection,
+        http: HttpClient::builder()
+            .timeout(Duration::from_secs(3))
+            .build()?,
+    };
     let app = Router::new()
         .route("/user/:id", get(get_user))
         .route("/signin/:id", get(get_arriving_customers))
-        .route("/signout/:id", get(get_leaving_customers))
         .route("/staying/:id", get(get_staying_customers))
         .route("/status/:id", post(change_appointment_status))
         .route("/create", post(create_appointment))
         .route("/stayed", post(stayed_customers))
-        .with_state(collection);
+        .with_state(shared_state);
     let addr = std::env::var("ADDR").unwrap_or_else(|_| "127.0.0.1:3000".into());
     let addr: SocketAddr = addr.parse()?;
     tracing::debug!("listening on {addr}");
@@ -45,10 +61,10 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-macro_rules! today_date {
-    () => {
-        chrono::offset::Local::now().date_naive().to_string()
-    };
+#[derive(Clone)]
+struct SharedState {
+    db: Collection<Appointment>,
+    http: HttpClient,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -56,18 +72,9 @@ struct Appointment {
     id: String,
     user_id: String,
     groomer_id: String,
-    start_date: String,
-    end_date: String,
+    start_date: DateTime,
+    end_date: DateTime,
     status: String,
-    pets: Vec<Pet>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct AppointmentInput {
-    user_id: String,
-    groomer_id: String,
-    start_date: String,
-    end_date: String,
     pets: Vec<Pet>,
 }
 
@@ -80,95 +87,195 @@ struct Pet {
     medical_info: String,
 }
 
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct UserEndpointOutput {
+    groomer_id: String,
+    groomer_name: String,
+    start_date: String,
+    end_date: String,
+    groomer_picture_url: String,
+    pet_names: Vec<String>,
+}
+
 async fn get_user(
     Path(user_id): Path<String>,
-    state: State<Collection<Appointment>>,
-) -> Result<Json<Vec<Appointment>>, ApiError> {
+    state: State<SharedState>,
+) -> Result<Json<Vec<UserEndpointOutput>>, ApiError> {
+    #[derive(Deserialize)]
+    struct UserReadEndpointOutput {
+        name: String,
+        picture_url: String,
+    }
+
+    // Check if user_id exists
+    if !state
+        .http
+        .get(format!("http://user:5000/read/{user_id}"))
+        .send()
+        .await
+        .map_err(|_| ApiError::InternalError)?
+        .status()
+        .as_u16()
+        == 200
+    {
+        return Err(ApiError::UserDoesNotExist);
+    }
+
     let filter = doc! {"user_id": user_id};
     let res = state
+        .db
         .find(filter, None)
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
     let res: Vec<_> = res
         .try_collect()
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
+    let groomers: HashSet<String> = res.iter().map(|app| app.groomer_id.clone()).collect();
+    let groomer_info: HashMap<String, UserReadEndpointOutput> = stream::iter(groomers)
+        .map(|id| {
+            let client = &state.http;
+            async move {
+                let json = json!({ "id": id });
+                let res: UserReadEndpointOutput = client
+                    .post("http://groomer:5000/read")
+                    .json(&json)
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                (id, res)
+            }
+        })
+        .buffer_unordered(3)
+        .collect()
+        .await;
+    let res = res
+        .into_iter()
+        .map(|app| UserEndpointOutput {
+            end_date: app.end_date.try_to_rfc3339_string().unwrap(),
+            groomer_name: groomer_info.get(&app.groomer_id).unwrap().name.clone(),
+            groomer_picture_url: groomer_info
+                .get(&app.groomer_id)
+                .unwrap()
+                .picture_url
+                .clone(),
+            groomer_id: app.groomer_id,
+            pet_names: app.pets.into_iter().map(|pet| pet.name).collect(),
+            start_date: app.start_date.try_to_rfc3339_string().unwrap(),
+        })
+        .collect();
     Ok(Json(res))
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct SignInOutput {
+    id: String,
+    user_name: String,
+    start_date: String,
+    end_date: String,
+    pets: Vec<Pet>,
 }
 
 async fn get_arriving_customers(
     Path(groomer_id): Path<String>,
-    state: State<Collection<Appointment>>,
-) -> Result<Json<Vec<Appointment>>, ApiError> {
-    let filter = doc! {"groomer_id": groomer_id, "status": "awaiting", "start_date": today_date!()};
-    let res = state
-        .find(filter, None)
-        .await
-        .map_err(|_| ApiError::DatabaseError)?;
-    let res: Vec<_> = res
-        .try_collect()
-        .await
-        .map_err(|_| ApiError::DatabaseError)?;
-    Ok(Json(res))
-}
+    state: State<SharedState>,
+) -> Result<Json<Vec<SignInOutput>>, ApiError> {
+    #[derive(Deserialize)]
+    struct UserReadEndpointOutput {
+        name: String,
+    }
 
-async fn get_leaving_customers(
-    Path(groomer_id): Path<String>,
-    state: State<Collection<Appointment>>,
-) -> Result<Json<Vec<Appointment>>, ApiError> {
-    let filter = doc! {"groomer_id": groomer_id, "status": "staying", "end_date": today_date!()};
+    let filter = doc! {"groomer_id": groomer_id, "status": "awaiting"};
     let res = state
+        .db
         .find(filter, None)
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
     let res: Vec<_> = res
         .try_collect()
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
+    let res = stream::iter(res)
+        .map(|app| {
+            let client = &state.http;
+            async move {
+                let res: UserReadEndpointOutput = client
+                    .get(format!("http://user:5000/read/{}", app.user_id))
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                SignInOutput {
+                    end_date: app.end_date.try_to_rfc3339_string().unwrap(),
+                    id: app.id,
+                    pets: app.pets,
+                    start_date: app.start_date.try_to_rfc3339_string().unwrap(),
+                    user_name: res.name,
+                }
+            }
+        })
+        .buffer_unordered(3)
+        .collect()
+        .await;
     Ok(Json(res))
 }
 
 async fn get_staying_customers(
     Path(groomer_id): Path<String>,
-    state: State<Collection<Appointment>>,
-) -> Result<Json<Vec<Appointment>>, ApiError> {
+    state: State<SharedState>,
+) -> Result<Json<Vec<SignInOutput>>, ApiError> {
+    #[derive(Deserialize)]
+    struct UserReadEndpointOutput {
+        name: String,
+    }
+
     let filter = doc! {"groomer_id": groomer_id, "status": "staying"};
     let res = state
+        .db
         .find(filter, None)
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
     let res: Vec<_> = res
         .try_collect()
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
+    let res = stream::iter(res)
+        .map(|app| {
+            let client = &state.http;
+            async move {
+                let res: UserReadEndpointOutput = client
+                    .get(format!("http://user:5000/read/{}", app.user_id))
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                SignInOutput {
+                    end_date: app.end_date.try_to_rfc3339_string().unwrap(),
+                    id: app.id,
+                    pets: app.pets,
+                    start_date: app.start_date.try_to_rfc3339_string().unwrap(),
+                    user_name: res.name,
+                }
+            }
+        })
+        .buffer_unordered(3)
+        .collect()
+        .await;
     Ok(Json(res))
 }
 
-#[derive(Serialize, Deserialize)]
-struct StayedCustomersInput {
-    groomer_id: String,
-    user_id: String,
-}
-
-async fn stayed_customers(
-    state: State<Collection<Appointment>>,
-    Json(payload): Json<StayedCustomersInput>,
-) -> Result<Json<Vec<Appointment>>, ApiError> {
-    let filter =
-        doc! {"groomer_id": payload.groomer_id, "user_id": payload.user_id, "status": "left"};
-    let res = state
-        .find(filter, None)
-        .await
-        .map_err(|_| ApiError::DatabaseError)?;
-    let res: Vec<_> = res
-        .try_collect()
-        .await
-        .map_err(|_| ApiError::DatabaseError)?;
-    Ok(Json(res))
-}
-
-#[derive(Serialize, Deserialize)]
-struct AppointmentStatus {
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StatusChangeInput {
     status: String,
 }
 
@@ -176,17 +283,18 @@ const STATUSES: [&'static str; 3] = ["awaiting", "staying", "left"];
 
 async fn change_appointment_status(
     Path(appointment_id): Path<String>,
-    state: State<Collection<Appointment>>,
-    Json(payload): Json<AppointmentStatus>,
+    state: State<SharedState>,
+    Json(payload): Json<StatusChangeInput>,
 ) -> Result<StatusCode, ApiError> {
     if !STATUSES.contains(&payload.status.as_str()) {
         return Err(ApiError::IncorrectStatus);
     }
     let filter = doc! {"id": appointment_id};
     let res = state
+        .db
         .find_one(filter.clone(), None)
         .await
-        .map_err(|_| ApiError::DatabaseError)?;
+        .map_err(|_| ApiError::InternalError)?;
     if let Some(old_appointment) = res {
         if old_appointment.status == STATUSES[1] && payload.status == STATUSES[0]
             || old_appointment.status == STATUSES[2] && payload.status == STATUSES[1]
@@ -194,49 +302,98 @@ async fn change_appointment_status(
             return Err(ApiError::IncorrectStatusFlow);
         }
         state
+            .db
             .update_one(filter, doc! {"$set": {"status": payload.status}}, None)
             .await
-            .map_err(|_| ApiError::DatabaseError)?;
+            .map_err(|_| ApiError::InternalError)?;
         Ok(StatusCode::OK)
     } else {
         Ok(StatusCode::NOT_FOUND)
     }
 }
 
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StayedCustomersInput {
+    groomer_id: String,
+    user_id: String,
+}
+
+async fn stayed_customers(
+    state: State<SharedState>,
+    Json(payload): Json<StayedCustomersInput>,
+) -> Result<StatusCode, ApiError> {
+    let filter =
+        doc! {"groomer_id": payload.groomer_id, "user_id": payload.user_id, "status": "left"};
+    let mut res = state
+        .db
+        .find(filter, None)
+        .await
+        .map_err(|_| ApiError::InternalError)?;
+    if res
+        .try_next()
+        .await
+        .map_err(|_| ApiError::InternalError)?
+        .is_some()
+    {
+        Ok(StatusCode::OK)
+    } else {
+        Ok(StatusCode::NOT_FOUND)
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateInput {
+    user_id: String,
+    groomer_id: String,
+    pet_info: Vec<Pet>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateOutput {
+    id: String,
+}
+
 async fn create_appointment(
-    state: State<Collection<Appointment>>,
-    Json(payload): Json<AppointmentInput>,
-) -> Result<Json<Appointment>, ApiError> {
+    state: State<SharedState>,
+    Json(payload): Json<CreateInput>,
+) -> Result<Json<CreateOutput>, ApiError> {
     let payload = Appointment {
-        end_date: payload.end_date,
+        end_date: DateTime::now(),
         groomer_id: payload.groomer_id,
-        id: Uuid::new_v4().to_string(),
-        pets: payload.pets,
-        start_date: payload.start_date,
+        id: cuid::cuid2(),
+        pets: payload.pet_info,
+        start_date: DateTime::now(),
         status: String::from("awaiting"),
         user_id: payload.user_id,
     };
-    state.insert_one(&payload, None).await.unwrap();
-    Ok(Json(payload))
+    state
+        .db
+        .insert_one(&payload, None)
+        .await
+        .map_err(|_| ApiError::InternalError)?;
+    Ok(Json(CreateOutput { id: payload.id }))
 }
 
 #[derive(Debug)]
 enum ApiError {
-    DatabaseError,
+    InternalError,
     IncorrectStatus,
     IncorrectStatusFlow,
+    UserDoesNotExist,
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
         let (status, error_message) = match self {
-            ApiError::DatabaseError => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "error querying database")
-            }
+            ApiError::InternalError => (StatusCode::INTERNAL_SERVER_ERROR, "internal server error"),
             ApiError::IncorrectStatus => (StatusCode::BAD_REQUEST, "incorrect status"),
             ApiError::IncorrectStatusFlow => (StatusCode::BAD_REQUEST, "incorrect status flow"),
+            ApiError::UserDoesNotExist => (StatusCode::NOT_FOUND, "user cannot be found"),
         };
-        let body = Json(json!({ "error": error_message }));
+        let body = Json(json!({ "message": error_message }));
         (status, body).into_response()
     }
 }
