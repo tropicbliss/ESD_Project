f7e3ad53eb4f1c80ec87f6405f971cdfe267ae07
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-03-18 18:31:43 +0800

    E

--- microservices/groomer/index.ts
+++ microservices/groomer/index.ts
@@ -40,15 +40,50 @@ app.post("/create", async (req, res) => {
         }
       }
     })
+    res.status(200);
+    res.send()
   } catch (err) {
-    console.error(err);
     res.status(400);
     res.send({ message: "message is invalid" });
     return;
   }
-  res.status(200);
-  res.send()
 });
+
+app.get("/search/keyword/:keyword", async (req, res) => {
+  const { keyword } = req.params;
+  try {
+    const result = await prisma.groomer.findMany({
+      where: {
+        name: {
+          contains: keyword
+        }
+      },
+      select: {
+        acceptedPets: true,
+        address: true,
+        capacity: true,
+        contactNo: true,
+        email: true,
+        name: true,
+        pictureUrl: true
+      }
+    })
+    if (result) {
+      const json = result.map((groomer) => {
+        const pets = groomer.acceptedPets.map((pet) => pet.petType.toString());
+        return { ...groomer, acceptedPets: pets }
+      })
+      res.status(200)
+      res.send(json)
+    } else {
+      res.status(404);
+      res.send()
+    }
+  } catch (err) {
+    res.status(400);
+    res.send({ message: "message is invalid" });
+  }
+})
 
 app.listen(5000, () => {
   console.log(`Censorer listening on port ${5000}`);
